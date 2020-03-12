1d23a00979d23ee6f99ec55a0b0ccf866fe68bc7
marder00 <61285197+marder00@users.noreply.github.com>
2020-03-12 10:47:34 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://i.pinimg.com/474x/63/7c/99/637c9949ebf504c1b492153d2bb3c775--black-and-white-portraits-white-lions.jpg"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
   <!-- END -->
